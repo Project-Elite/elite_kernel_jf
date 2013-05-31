f84054f9eeb7981d623786e5f6825ac9a2b73005
Matt Filetto <matt.filetto@gmail.com>
2013-05-30 23:38:02 -0700

    GPU: revert changes to GPU low/med/high values
    Change-Id: I6c7e16c17ee3452ccdb22cb32019014752c065c4

--- arch/arm/mach-msm/clock-8960.c
+++ arch/arm/mach-msm/clock-8960.c
@@ -3583,13 +3583,13 @@ static struct clk_freq_tbl clk_tbl_gfx3d_8930ab[] = {
 
 static unsigned long fmax_gfx3d_8064ab[VDD_DIG_NUM] = {
 	[VDD_DIG_LOW]     = 128000000,
-	[VDD_DIG_NOMINAL] = 320000000,
+	[VDD_DIG_NOMINAL] = 325000000,
 	[VDD_DIG_HIGH]    = 500000000
 };
 
 static unsigned long fmax_gfx3d_8064[VDD_DIG_NUM] = {
 	[VDD_DIG_LOW]     = 128000000,
-	[VDD_DIG_NOMINAL] = 320000000,
+	[VDD_DIG_NOMINAL] = 325000000,
 	[VDD_DIG_HIGH]    = 500000000
 };
 
