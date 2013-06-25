11394722160de61b5ffcc856947777ad1cdce177
buckmarble <forrestmarble@gmail.com>
2013-06-25 19:59:39 -0400

    fix initialization of max gpu/cpu freqs on boot

--- Ramdisks/AOSP_INTL/sbin/post-init.sh
+++ Ramdisks/AOSP_INTL/sbin/post-init.sh
@@ -1,11 +1,11 @@
 #!/system/bin/sh
 
 #set gpu max_clk rate
-echo 450000000 > /sys/devices/platform/kgsl-3d0.0/kgsl/kgsl-3d0/max_gpuclk
+echo 627000000 > /sys/devices/platform/kgsl-3d0.0/kgsl/kgsl-3d0/max_gpuclk
 
 #Set governor items
 echo 378000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq;
-echo 1890000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq;
+echo 2322000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq;
 echo "interactive" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor;
 echo "interactive" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor;
 echo "interactive" > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor;
