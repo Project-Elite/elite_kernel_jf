4c9b7e39b3008956992539abe088d20171130781
buckmarble <forrestmarble@gmail.com>
2013-06-28 18:56:36 -0400

    take these back down too

--- Ramdisks/AOSP_SPR/sbin/post-init.sh
+++ Ramdisks/AOSP_SPR/sbin/post-init.sh
@@ -3,7 +3,7 @@
 echo 450000000 > /sys/devices/platform/kgsl-3d0.0/kgsl/kgsl-3d0/max_gpuclk
 
 #Set governor items
-echo 2322000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq;
+echo 1890000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq;
 echo 1 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_booted;
 
 echo $(date) END of post-init.sh
