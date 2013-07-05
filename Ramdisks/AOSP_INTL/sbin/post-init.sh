47d66b908853fb34bd81290c1700213438cfd3f1
buckmarble <forrestmarble@gmail.com>
2013-07-05 11:16:18 -0400

    GPU: Lets simplify the OC. Problem was you couldnt change the freq on the fly with an app like trickster. Second, GPU overclock REALLY is NOT necessary. With this method default freq is 400 and OC to 600 and will change on the fly with trickster

--- Ramdisks/AOSP_INTL/sbin/post-init.sh
+++ Ramdisks/AOSP_INTL/sbin/post-init.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
 
-echo 500000000 > /sys/devices/platform/kgsl-3d0.0/kgsl/kgsl-3d0/max_gpuclk
+echo 600000000 > /sys/devices/platform/kgsl-3d0.0/kgsl/kgsl-3d0/max_gpuclk
 
 #Set governor items
 echo 1890000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq;
--- arch/arm/mach-msm/board-8064-gpu.c
+++ arch/arm/mach-msm/board-8064-gpu.c
@@ -221,22 +221,12 @@ static struct kgsl_device_iommu_data kgsl_3d0_iommu_data[] = {
 static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.pwrlevel = {
 		{
-			.gpu_freq = 500000000,
-			.bus_freq = 4,
-			.io_fraction = 0,
-		},
-		{
-			.gpu_freq = 450000000,
+			.gpu_freq = 600000000,
 			.bus_freq = 4,
 			.io_fraction = 0,
 		},
 		{
 			.gpu_freq = 400000000,
-			.bus_freq = 4,
-			.io_fraction = 0,
-		},
-		{
-			.gpu_freq = 320000000,
 			.bus_freq = 3,
 			.io_fraction = 33,
 		},
@@ -256,7 +246,7 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 		},
 	},
 	.init_level = 1,
-	.num_levels = 7,
+	.num_levels = 5,
 	.set_grp_async = NULL,
 	.idle_timeout = HZ/10,
 	.nap_allowed = true,
@@ -292,7 +282,7 @@ void __init apq8064_init_gpu(void)
 	unsigned int version = socinfo_get_version();
 
 	if (cpu_is_apq8064ab())
-		kgsl_3d0_pdata.pwrlevel[0].gpu_freq = 500000000;
+		kgsl_3d0_pdata.pwrlevel[0].gpu_freq = 600000000;
 	if (SOCINFO_VERSION_MAJOR(version) == 2) {
 		kgsl_3d0_pdata.chipid = ADRENO_CHIPID(3, 2, 0, 2);
 	} else {
