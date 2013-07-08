8484a5d7d394c9bb8bca6c3f4f6a18bee316a5e1
buckmarble <forrestmarble@gmail.com>
2013-07-07 21:14:44 -0400

    fix gpu max freq in post init script

--- Ramdisks/AOSP_ATT/sbin/post-init.sh
+++ Ramdisks/AOSP_ATT/sbin/post-init.sh
@@ -1,7 +1,7 @@
 #!/sbin/busybox sh
 #
 #Set Max Mhz for GPU
-echo 450000000 > /sys/devices/platform/kgsl-3d0.0/kgsl/kgsl-3d0/max_gpuclk
+echo 600000000 > /sys/devices/platform/kgsl-3d0.0/kgsl/kgsl-3d0/max_gpuclk
 
 #Set Max Mhz speed and booted flag to set Super Max
 echo 1890000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq;
