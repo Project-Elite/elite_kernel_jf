53cee2926e4ce63dd7515605d87ce76194973ad3
buckmarble <forrestmarble@gmail.com>
2013-10-16 16:22:29 -0400

    Bluetooth: update some driver changes and increase low board voltage

---
+++ build-spr-pe.sh
@@ -0,0 +1,80 @@
+#!/bin/sh
+export KERNELDIR=`readlink -f .`
+export PARENT_DIR=`readlink -f ..`
+export INITRAMFS_DEST=$KERNELDIR/kernel/usr/initramfs
+export INITRAMFS_SOURCE=`readlink -f ..`/elite_kernel_jf/Ramdisks/PE
+export CONFIG_AOSP_BUILD=y
+export PACKAGEDIR=$KERNELDIR/Packages/AOSP
+# enable ccache
+export USE_CCACHE=1
+#Enable FIPS mode
+export USE_SEC_FIPS_MODE=true
+export ARCH=arm
+echo "### SPR KERNEL BUILD ###"
+echo "Setting compiler toolchain..."
+export CROSS_COMPILE=/home/forrest/kernel/android-toolchain-eabi/bin/arm-eabi-
+
+time_start=$(date +%s.%N)
+
+echo "Remove old Package Files"
+rm -rf $PACKAGEDIR/* > /dev/null 2>&1
+
+echo "Setup Package Directory"
+mkdir -p $PACKAGEDIR/system/lib/modules
+mkdir -p $PACKAGEDIR/system/etc/init.d
+
+echo "Create initramfs dir"
+mkdir -p $INITRAMFS_DEST
+
+echo "Remove old initramfs dir"
+rm -rf $INITRAMFS_DEST/* > /dev/null 2>&1
+
+echo "Copy new initramfs dir"
+cp -R $INITRAMFS_SOURCE/* $INITRAMFS_DEST
+
+echo "chmod initramfs dir"
+chmod -R g-w $INITRAMFS_DEST/*
+rm $(find $INITRAMFS_DEST -name EMPTY_DIRECTORY -print) > /dev/null 2>&1
+rm -rf $(find $INITRAMFS_DEST -name .git -print)
+
+echo "Remove old zImage"
+rm $PACKAGEDIR/zImage > /dev/null 2>&1
+rm arch/arm/boot/zImage > /dev/null 2>&1
+
+echo "Make the kernel"
+make VARIANT_DEFCONFIG=jf_spr_defconfig SELINUX_DEFCONFIG=jfselinux_defconfig SELINUX_LOG_DEFCONFIG=jfselinux_log_defconfig elite_kernel_defconfig
+
+HOST_CHECK=`uname -n`
+if [ $HOST_CHECK = 'ktoonsez-VirtualBox' ] || [ $HOST_CHECK = 'task650-Underwear' ]; then
+	echo "Ktoonsez/task650 24!"
+	make -j24
+else
+	echo "Others! - " + $HOST_CHECK
+	make -j`grep 'processor' /proc/cpuinfo | wc -l`
+fi;
+
+echo "Copy modules to Package"
+cp -a $(find . -name *.ko -print |grep -v initramfs) $PACKAGEDIR/system/lib/modules/
+cp Packages/03elite $PACKAGEDIR/system/etc/init.d/03elite
+
+if [ -e $KERNELDIR/arch/arm/boot/zImage ]; then
+	echo "Copy zImage to Package"
+	cp arch/arm/boot/zImage $PACKAGEDIR/zImage
+
+	echo "Make boot.img"
+	./mkbootfs $INITRAMFS_DEST | gzip > $PACKAGEDIR/ramdisk.gz
+	./mkbootimg --cmdline 'console = null androidboot.hardware=qcom user_debug=31 zcache' --kernel $PACKAGEDIR/zImage --ramdisk $PACKAGEDIR/ramdisk.gz --base 0x80200000 --pagesize 2048 --ramdisk_offset 0x02000000 --output $PACKAGEDIR/boot.img 
+	export curdate=`date "+%m-%d-%Y"`
+	cd $PACKAGEDIR
+	cp -R ../META-INF-NOLOKI ./META-INF
+	rm ramdisk.gz
+	rm zImage
+	zip -r ../EliteKernel-JFspr-$curdate.zip .
+	cd $KERNELDIR
+else
+	echo "KERNEL DID NOT BUILD! no zImage exist"
+fi;
+
+time_end=$(date +%s.%N)
+echo -e "${BLDYLW}Total time elapsed: ${TCTCLR}${TXTGRN}$(echo "($time_end - $time_start) / 60"|bc ) ${TXTYLW}minutes${TXTGRN} ($(echo "$time_end - $time_start"|bc ) ${TXTYLW}seconds) ${TXTCLR}"
+
