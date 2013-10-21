a238d7ba0b0cdef8df20b5aba4f3667f6d36c4d4
buckmarble <forrestmarble@gmail.com>
2013-10-20 22:23:27 -0400

    Revert "add auto hotplug"
    This reverts commit f2618493fae5567fc659225860db4f0ee544b654.

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -32,11 +32,6 @@
 
 #include <trace/events/power.h>
 
-#ifdef CONFIG_ARM_AUTO_HOTPLUG
-static unsigned int Lenable_auto_hotplug = 0;
-extern void apenable_auto_hotplug(bool state);
-#endif
-
 /**
  * The "cpufreq driver" - the arch- or hardware-dependent low
  * level driver of CPUFreq support, and its spinlock. This lock
@@ -628,23 +623,6 @@ static ssize_t show_bios_limit(struct cpufreq_policy *policy, char *buf)
 	return sprintf(buf, "%u\n", policy->cpuinfo.max_freq);
 }
 
-#ifdef CONFIG_ARM_AUTO_HOTPLUG
-static ssize_t show_enable_auto_hotplug(struct cpufreq_policy *policy, char *buf) 
-{
-	return sprintf(buf, "%u\n", Lenable_auto_hotplug);
-}
-static ssize_t store_enable_auto_hotplug(struct cpufreq_policy *policy, 
-			const char *buf, size_t count)
-{
-	unsigned int val = 0;
-	unsigned int ret;
-	ret = sscanf(buf, "%u", &val);
-	Lenable_auto_hotplug = val;
-	apenable_auto_hotplug((bool) Lenable_auto_hotplug);
-	return count;
-}
-#endif
-
 #ifdef CONFIG_CPU_VOLTAGE_TABLE
 
 extern ssize_t acpuclk_get_vdd_levels_str(char *buf);
@@ -722,10 +700,6 @@ cpufreq_freq_attr_rw(scaling_min_freq);
 cpufreq_freq_attr_rw(scaling_max_freq);
 cpufreq_freq_attr_rw(scaling_governor);
 cpufreq_freq_attr_rw(scaling_setspeed);
-#ifdef CONFIG_ARM_AUTO_HOTPLUG
-cpufreq_freq_attr_rw(enable_auto_hotplug);
-#endif
-
 #ifdef CONFIG_CPU_VOLTAGE_TABLE
 define_one_global_rw(vdd_levels);
 #endif
@@ -743,9 +717,6 @@ static struct attribute *default_attrs[] = {
 	&scaling_driver.attr,
 	&scaling_available_governors.attr,
 	&scaling_setspeed.attr,
-#ifdef CONFIG_ARM_AUTO_HOTPLUG
-	&enable_auto_hotplug.attr,
-#endif
 	NULL
 };
 
